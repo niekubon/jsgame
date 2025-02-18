1e05195dc52a7cc105c3b42d1d99eb255899664f
niekubon <166243445+niekubon@users.noreply.github.com>
2025-02-18 19:30:58 +0100

    Add files via upload

---
+++ jordanscript_1.js
@@ -0,0 +1,9 @@
+//życie
+function healthminus10(){
+    let health = document.getElementById("health")
+    health.value -= 10;
+}
+function healthplus10(){
+    let health = document.getElementById("health")
+    health.value += 10;
+}
